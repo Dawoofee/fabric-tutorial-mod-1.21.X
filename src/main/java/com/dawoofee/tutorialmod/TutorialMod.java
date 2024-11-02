453c54409802977c8b4add34a7c78026374566de
SebastianUngard <sebastianungard@gmail.com>
2024-11-02 17:37:02 -0600

    Added sapphire and raw sapphire

--- src/main/java/com/dawoofee/tutorialmod/TutorialMod.java
+++ src/main/java/com/dawoofee/tutorialmod/TutorialMod.java
@@ -1,7 +1,9 @@
 package com.dawoofee.tutorialmod;
 
+
 import net.fabricmc.api.ModInitializer;
 
+import com.dawoofee.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +13,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/dawoofee/tutorialmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package com.dawoofee.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import com.dawoofee.tutorialmod.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item SAPPHIRE = registerItem("sapphire", new Item(new Item.Settings()));
+    public static final Item RAW_SAPPHIRE = registerItem("raw_sapphire", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(SAPPHIRE);
+            entries.add(RAW_SAPPHIRE);
+        });
+    }
+}
